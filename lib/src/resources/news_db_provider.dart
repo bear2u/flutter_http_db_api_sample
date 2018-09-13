2f58eae00424311221329fb7b644cf5e33c76b3e
dev <tommykim1981@gmail.com>
2018-09-13 19:13:34 +0900

    init

---
+++ lib/src/resources/news_db_provider.dart
@@ -0,0 +1,59 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'dart:async';
+import '../models/item_model.dart';
+
+class NewsDbProvider {
+  Database db;
+  
+  void init() async{
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, "items.db");
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database newDb, int version) {
+        newDb.execute("""
+          CREATE TABLE Items
+          (
+            id integer primary key,
+            type text,
+            by  text,
+            time integer,
+            text text,
+            parent text,
+            kids blob,
+            dead integer,
+            deleted integer,
+            url text,
+            score integer,
+            title text,
+            descendants integer
+          )
+        """);
+      }
+    );
+  }
+
+  Future<ItemModel> fetchItem(int id) async {
+    final maps = await db.query(
+      "Items",
+      columns: null,
+      where: "id = ?",
+      whereArgs: [id],
+    );
+
+    if (maps.length > 0) {
+      return ItemModel.fromDb(maps.first);
+    }
+
+    return null;
+  }
+
+  Future<int> addItem(ItemModel item) async {
+    return db.insert("Items", item.toMap());
+  }
+}
+
